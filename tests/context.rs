d46048779767b513e51fee86792200f227393db7
Jamie Hill-Daniel <jamie@hill-daniel.co.uk>
2022-03-01 22:24:49 +0000

    Hold context lock for whole test

--- tests/context.rs
+++ tests/context.rs
@@ -1,13 +1,15 @@
 use pwn::context::Bits::SixtyFour;
 use pwn::context::Endianness::Little;
-use pwn::context::{context, context_mut, AMD64, I386};
+use pwn::context::{context_mut, AMD64, I386};
 
 /// Test `Context`
 #[test]
 fn test_context() {
-    assert_eq!(context().arch, I386);
-    context_mut().set_arch(AMD64);
-    assert_eq!(context().arch, AMD64);
-    assert_eq!(context().endian, Little);
-    assert_eq!(context().bits, SixtyFour);
+    let mut ctx = context_mut();
+    assert_eq!(ctx.arch, I386);
+    assert_eq!(ctx.arch, I386);
+    ctx.set_arch(AMD64);
+    assert_eq!(ctx.arch, AMD64);
+    assert_eq!(ctx.endian, Little);
+    assert_eq!(ctx.bits, SixtyFour);
 }
